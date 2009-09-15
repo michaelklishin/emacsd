22e33a217a9c477728e6c7595c5afbd66e0454d2
Dmitriy Dzema <dmitriy@dzema.name>
2009-09-15 18:20:26 +1100

    Revert back paren-highlight

--- custom-variables.el
+++ custom-variables.el
@@ -1,8 +1,8 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 202 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -44,12 +44,14 @@
  '(server-mode nil)
  '(server-raise-frame nil)
  '(size-indication-mode t)
- '(standard-indent 2))
+ '(standard-indent 2)
+ '(column-number-mode t)
+ '(show-paren-mode t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#040404" :foreground "#FFFFFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 120 :width condensed :foundry "apple" :family "Monaco"))))
  '(Custom-mode-default ((t (:inherit autoface-default :height 140 :family "DejaVu Sans Mono"))) t)
  '(font-lock-comment-face ((t (:foreground "#9933CC")))))
