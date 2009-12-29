a24216f95b3f9c83d7ccb1fb3fe53a6fe224e920
Michael S. Klishin <michael@novemberain.com>
2009-12-29 14:46:34 +0200

    Remove some duplication in precompilation script
     * Both zsh and bash can handle low-nested files compilation the same way

--- tools/bin/precompile.sh
+++ tools/bin/precompile.sh
@@ -2,18 +2,16 @@
 
 COMPILATION_RUN_FLAGS="--directory . --directory ~/emacsd -batch -f batch-byte-compile"
 
+emacs $COMPILATION_RUN_FLAGS *.el emacs $COMPILATION_RUN_FLAGS  initializers/*.el themes/*.el
+
 case `printenv SHELL` in
     /bin/zsh)
-        emacs $COMPILATION_RUN_FLAGS  *.el initializers/*.el themes/*.el
         emacs $COMPILATION_RUN_FLAGS  bundles/**/*.el
         ;;
     /bin/bash)
-        emacs $COMPILATION_RUN_FLAGS  *.el
         for bundle in bundles/*; do
             emacs $COMPILATION_RUN_FLAGS  $bundle/*.el
         done
-        emacs $COMPILATION_RUN_FLAGS  initializers/*.el
-        emacs $COMPILATION_RUN_FLAGS  themes/*.el
         ;;
 esac
 
