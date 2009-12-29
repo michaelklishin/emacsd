0be759fc0f9d74f55e5823235fde46d53be92130
Michael S. Klishin <michael@novemberain.com>
2009-12-29 14:49:11 +0200

    Rearrange bundles load order

--- initializers/bundles_initializer.el
+++ initializers/bundles_initializer.el
@@ -23,19 +23,32 @@
 (elbundle/load-elbundle "emacs-lisp")
 (elbundle/load-elbundle "etest")
 
+(elbundle/load-elbundle "org")
+
 (elbundle/load-elbundle "linkify")
-(elbundle/load-elbundle "ruby")
 (elbundle/load-elbundle "git")
+(elbundle/load-elbundle "hg")
 
-(elbundle/load-elbundle "datamapper")
-(elbundle/load-elbundle "merb")
-
+(elbundle/load-elbundle "ruby")
 (elbundle/load-elbundle "rspec")
 (elbundle/load-elbundle "cucumber")
 (elbundle/load-elbundle "rake")
 
-(elbundle/load-elbundle "git")
-(elbundle/load-elbundle "hg")
+(elbundle/load-elbundle "c")
+(elbundle/load-elbundle "cpp")
+(elbundle/load-elbundle "sql")
+(elbundle/load-elbundle "erlang")
+(elbundle/load-elbundle "python")
+(elbundle/load-elbundle "scala")
+(elbundle/load-elbundle "clojure")
+(elbundle/load-elbundle "slime")
+;; (elbundle/load-elbundle "haskell")
+(elbundle/load-elbundle "perl")
+(elbundle/load-elbundle "javascript")
+(elbundle/load-elbundle "shell-script")
+
+(elbundle/load-elbundle "datamapper")
+(elbundle/load-elbundle "merb")
 
 (elbundle/load-elbundle "yaml")
 (elbundle/load-elbundle "haml")
@@ -50,27 +63,10 @@
 (elbundle/load-elbundle "webtools")
 (elbundle/load-elbundle "top-mode")
 
-(elbundle/load-elbundle "sql")
-(elbundle/load-elbundle "org")
-
-(elbundle/load-elbundle "python")
-(elbundle/load-elbundle "scala")
-(elbundle/load-elbundle "clojure")
-(elbundle/load-elbundle "slime")
-;; (elbundle/load-elbundle "haskell")
-(elbundle/load-elbundle "perl")
-;; (elbundle/load-elbundle "javascript")
-(elbundle/load-elbundle "shell-script")
-;; (elbundle/load-elbundle "treetop-mode")
-
 ;; for extensions to Emacs itself
 (elbundle/load-elbundle "emacs")
 
-(elbundle/load-elbundle "c")
-(elbundle/load-elbundle "cpp")
-
 ;; (elbundle/load-elbundle "apache-mode")
-(elbundle/load-elbundle "erlang")
 
 (elbundle/load-elbundle "growl")
 (elbundle/load-elbundle "ack")
