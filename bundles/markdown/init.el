76edab0a2ddbde4da9b08d8c8a31b65a91022b51
Michael S. Klishin <michael@novemberain.com>
2010-02-07 20:50:55 +0200

    Use markdown-mode for .md

--- bundles/markdown/init.el
+++ bundles/markdown/init.el
@@ -3,3 +3,4 @@
 ;; (yas/load-directory "~/emacsd/bundles/markdown/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
