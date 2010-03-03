d99d69f922114d3c7c33bdb4db23425bb7871629
Michael S. Klishin <michael@novemberain.com>
2010-03-03 12:21:33 +0200

    Use js-mode, which is packaged in recent versions of Emacs

--- bundles/javascript/init.el
+++ bundles/javascript/init.el
@@ -1,6 +1,7 @@
-(load "js2-mode")
-(require 'js2-mode)
+;; (load "js-mode")
+;; (require 'js2-mode)
+;; (require 'js-mode)
 
 (yas/load-directory "~/emacsd/bundles/javascript/snippets")
 
-(add-to-list 'auto-mode-alist '("\\.js" js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.js" js-mode))
