157d79629d339b193440ca91527f72184fa2e49f
Michael S. Klishin <michael@novemberain.com>
2010-03-12 16:38:02 +0200

    Clean up

--- bundles/javascript/init.el
+++ bundles/javascript/init.el
@@ -1,7 +1,3 @@
-;; (load "js-mode")
-;; (require 'js2-mode)
-;; (require 'js-mode)
-
 (yas/load-directory "~/emacsd/bundles/javascript/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.json" js-mode))
