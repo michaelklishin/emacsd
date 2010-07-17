01363aa6821a873f1cb6b57e58697b09c6b87958
Michael S. Klishin <michael@novemberain.com>
2010-07-18 01:38:15 +0400

    First snippet for Clojure: (ns ...)

--- bundles/slime/init.el
+++ bundles/slime/init.el
@@ -5,14 +5,18 @@
 (require 'swank-clojure-autoload)
 
 (swank-clojure-config
- (setq swank-clojure-jar-path "~/lib/lang/clojure.jar")
+ (setq swank-clojure-jar-path "/usr/local/Cellar/clojure/1.1.0/clojure.jar")
  (setq swank-clojure-extra-classpaths
-       (list "~/lib/lang/clojure-contrib.jar")))
+       (list "/usr/local/Cellar/clojure-contrib/1.1.0/clojure-contrib.jar")))
 
 (eval-after-load "slime"
   '(progn (slime-setup '(slime-repl))))
 
-(setq inferior-lisp-program "~java -jar /Users/antares/lib/lang/clojure.jar")
+(setq inferior-lisp-program "/usr/local/bin/clj")
 
 (require 'slime)
 (slime-setup)
+
+
+;; load bundle snippets
+(yas/load-directory "~/emacsd/bundles/clojure/snippets")
