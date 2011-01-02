c2a2c3545e3c38f2b6b29fdfc94318a65ca42615
Michael S. Klishin <michael@novemberain.com>
2011-01-02 19:13:50 +0300

    Byte-compile js2-mode

--- bundles/javascript/init.el
+++ bundles/javascript/init.el
@@ -1,4 +1,5 @@
 (yas/load-directory "~/emacsd/bundles/javascript/snippets")
 
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.json" javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.js" js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json" js2-mode))
