faa5f9162b83796c6f2ef70335f26e59f329388a
Michael S. Klishin <michael@novemberain.com>
2011-01-02 11:31:23 +0300

    Enforce Python mode for wscript (Waf) and SConstruct (scons) files

--- bundles/python/init.el
+++ bundles/python/init.el
@@ -3,5 +3,8 @@
 ;;(require 'pycomplete)
 ;;(require 'doctest-mode)
 
+(add-to-list 'auto-mode-alist '("wscript$" . python-mode))
+(add-to-list 'auto-mode-alist '("SConstruct$" . python-mode))
+
 ;; load bundle snippets
 (yas/load-directory "~/emacsd/bundles/python/snippets")
