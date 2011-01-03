04e23f42af1c341b8e5516948d05424a0aa434b4
Michael S. Klishin <michael@novemberain.com>
2011-01-03 11:42:22 +0300

    Properly autoload js2-mode before we use it for .js and .json files

--- bundles/javascript/init.el
+++ bundles/javascript/init.el
@@ -1,5 +1,8 @@
 (yas/load-directory "~/emacsd/bundles/javascript/snippets")
 
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js" js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json" js2-mode))
+
+
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
