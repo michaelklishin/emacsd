9b99bca5903a5f530cea2ce3b0f4dc71eedee325
Michael S. Klishin <michael@novemberain.com>
2011-01-04 16:46:44 +0300

    Use shell-script-mode for files ending in zshenv and zprofile

--- bundles/shell-script/init.el
+++ bundles/shell-script/init.el
@@ -1,2 +1,7 @@
 (yas/load-directory "~/emacsd/bundles/shell-script/snippets")
+
 (add-to-list 'auto-mode-alist '("\\.sh$" . shell-script-mode))
+
+(add-to-list 'auto-mode-alist '("\\zshenv$" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\zprofile$" . shell-script-mode))
+
