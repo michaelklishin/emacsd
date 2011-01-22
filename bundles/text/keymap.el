191ca6bb51fafda0b9915d4278bc97c165a1d547
Michael S. Klishin <michael@novemberain.com>
2011-01-22 16:11:23 +0300

    Add a way to switch Meta key on OS X (so that I can type in Portuguese and Italian)
     * Option is used to type Western Latin characters in English layout on OS X.
       See http://bit.ly/g0Bvp1 for details.

--- bundles/text/keymap.el
+++ bundles/text/keymap.el
@@ -13,3 +13,30 @@
 ;; Bind rgrep to `\C-c\C-l'
 (global-set-key "\C-c\C-l" 'rgrep)
 (global-set-key "\C-x\M-k" 'wipe-out-all-buffers)
+
+
+;;
+;; Option or Command as Meta
+;;
+
+(defun mac-use-command-as-meta ()
+  (interactive)
+  (setq mac-option-key-is-meta nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil))
+
+(defun mac-use-option-as-meta ()
+  (interactive)
+  (setq mac-option-key-is-meta t)
+  (setq mac-command-key-is-meta nil)
+  (setq mac-command-modifier nil)
+  (setq mac-option-modifier 'meta))
+
+(defun mac-flip-meta-key ()
+  (interactive)
+  (if mac-option-key-is-meta
+      (mac-use-command-as-meta)
+    (mac-use-option-as-meta)))
+
+(global-set-key "\C-c§" 'mac-flip-meta-key)
