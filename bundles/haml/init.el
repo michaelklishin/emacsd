d292f5d63aade2dbacc3aed04297622100ce8842
Michael S. Klishin <michael@novemberain.com>
2011-02-15 12:44:32 +0300

    Load sass-mode

--- bundles/haml/init.el
+++ bundles/haml/init.el
@@ -1,6 +1,8 @@
 (load "haml-mode")
+(load "sass-mode")
 
 ;; load bundle snippets
 (yas/load-directory "~/emacsd/bundles/haml/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
