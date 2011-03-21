4f850365adf17735ce7db64c883e7a0460d33c10
Michael S. Klishin <michael@novemberain.com>
2011-03-21 03:55:44 +0300

    A few notes on how to get started with highlight-parenthesis mode

--- bundles/clojure/init.el
+++ bundles/clojure/init.el
@@ -4,3 +4,15 @@
 ;; (yas/load-directory "~/emacsd/bundles/clojure/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+
+;; (setq hl-paren-colors
+;;       '(;"#8f8f8f" ; this comes from Zenburn
+;;                                         ; and I guess I'll try to make the far-outer parens look like this
+;;         "orange1" "yellow1" "greenyellow" "green1"
+;;         "springgreen1" "cyan1" "slateblue1" "magenta1" "purple"))
+
+;; (define-globalized-minor-mode global-highlight-parentheses-mode
+;;   highlight-parentheses-mode
+;;   (lambda ()
+;;     (highlight-parentheses-mode t)))
+;; (global-highlight-parentheses-mode t)
