d5f34f6ece14b2fd14d22616904122afc6250010
Michael S. Klishin <michael@novemberain.com>
2011-04-23 01:34:02 +0400

    Use tuareg-mode for .cml files, too

--- bundles/ocaml/init.el
+++ bundles/ocaml/init.el
@@ -4,4 +4,4 @@
 ;; tuareg-library-path to "/usr/local/Cellar/objective-caml/lib/ocaml"
 
 
-(add-to-list 'auto-mode-alist '("\\.ml$" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.c?ml" . tuareg-mode))
