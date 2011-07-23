74b66b6c1c991222a2453ad536b96fa32546be09
Michael S. Klishin <michael@novemberain.com>
2011-07-23 12:30:14 +0400

    Use ruby-mode for Vagrantfile

--- bundles/ruby/init.el
+++ bundles/ruby/init.el
@@ -13,5 +13,6 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("config.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("*ruby-scratch*" . ruby-mode))
