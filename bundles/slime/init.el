a5cc329c02dfea6ac6324a7989150276cc166566
Michael S. Klishin <michael@novemberain.com>
2011-08-16 10:06:16 +0400

    SLIME should not load clojure-mode

--- bundles/slime/init.el
+++ bundles/slime/init.el
@@ -1,12 +1,6 @@
 (add-to-list 'load-path "~/emacsd/bundles/slime/swank-clojure")
 (add-to-list 'load-path "~/emacsd/bundles/slime/slime")
 
-(require 'clojure-mode)
-;; (require 'swank-clojure-autoload)
-
-;; (swank-clojure-config
-;;  (setq swank-clojure-jar-path "/usr/local/Cellar/clojure/1.2.1/clojure.jar"))
-
 (eval-after-load "slime"
   '(progn (slime-setup '(slime-repl))))
 
@@ -14,7 +8,3 @@
 
 (require 'slime)
 (slime-setup)
-
-
-;; load bundle snippets
-(yas/load-directory "~/emacsd/bundles/clojure/snippets")
