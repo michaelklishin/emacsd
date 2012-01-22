5b92ca495c3b1c51f75039bbfe7a27078d1c46cd
Michael S. Klishin <michael@novemberain.com>
2012-01-22 16:35:24 +0400

    Create Clojure scratch by default

--- initializers/scratches_initializer.el
+++ initializers/scratches_initializer.el
@@ -1,2 +1,5 @@
 (let ((default-major-mode 'ruby-mode))
       (set-buffer-major-mode (get-buffer-create "*ruby-scratch*")))
+
+(let ((default-major-mode 'clojure-mode))
+      (set-buffer-major-mode (get-buffer-create "*clojure-scratch*")))
