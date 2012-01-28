d3577c06b729c376fe1e8b432ee5c5965a076be0
Michael S. Klishin <michael@novemberain.com>
2012-01-28 22:26:59 +0400

    Use highlight-parentheses-mode for Emacs Lisp

--- bundles/emacs-lisp/init.el
+++ bundles/emacs-lisp/init.el
@@ -1,2 +1,10 @@
 ;; load bundle snippets
 (yas/load-directory "~/emacsd/bundles/emacs-lisp/snippets")
+
+(require 'highlight-parentheses)
+
+(highlight-parentheses-mode)
+
+(autoload 'highlight-parentheses-mode "highlight parenthesis"
+  "Highlights parenthesis in Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook          (lambda () (highlight-parentheses-mode +1)))
