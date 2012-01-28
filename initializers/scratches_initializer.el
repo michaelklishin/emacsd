69c719d21d55801f41db7d91ddc1205a324b1af2
Michael S. Klishin <michael@novemberain.com>
2012-01-28 22:29:57 +0400

    Scratches initializer has to setq global default-major-mode

--- initializers/scratches_initializer.el
+++ initializers/scratches_initializer.el
@@ -1,5 +1,11 @@
-(let ((major-mode 'ruby-mode))
-      (set-buffer-major-mode (get-buffer-create "*ruby-scratch*")))
+(setq default-major-mode 'clojure-mode)
+(set-buffer-major-mode (get-buffer-create "*clojure-scratch*"))
 
-(let ((major-mode 'clojure-mode))
-      (set-buffer-major-mode (get-buffer-create "*clojure-scratch*")))
+(setq default-major-mode 'java-mode)
+(set-buffer-major-mode (get-buffer-create "*java-scratch*"))
+
+(setq default-major-mode 'ruby-mode)
+(set-buffer-major-mode (get-buffer-create "*ruby-scratch*"))
+
+(setq default-major-mode 'org-mode)
+(set-buffer-major-mode (get-buffer-create "*org-scratch*"))
