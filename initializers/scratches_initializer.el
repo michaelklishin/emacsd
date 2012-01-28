cdd6938cbc7b6b33df01e6fc7f0c8c7d65e31864
Michael S. Klishin <michael@novemberain.com>
2012-01-28 20:55:16 +0400

    Use major-mode instead of default-major-mode that is deprecated in Emacs 23.2
    According to the Emacs Lisp Reference

--- initializers/scratches_initializer.el
+++ initializers/scratches_initializer.el
@@ -1,5 +1,5 @@
-(let ((default-major-mode 'ruby-mode))
+(let ((major-mode 'ruby-mode))
       (set-buffer-major-mode (get-buffer-create "*ruby-scratch*")))
 
-(let ((default-major-mode 'clojure-mode))
+(let ((major-mode 'clojure-mode))
       (set-buffer-major-mode (get-buffer-create "*clojure-scratch*")))
