f1fb07539d89a556a8302f62b674c48049138d91
Michael S. Klishin <michael@novemberain.com>
2012-04-07 17:44:36 +0400

    There are no Mustache snippets yet

--- bundles/mustache/init.el
+++ bundles/mustache/init.el
@@ -1,6 +1,6 @@
 (require 'mustache-mode)
 
 ;; load bundle snippets
-(yas/load-directory "~/emacsd/bundles/mustache/snippets")
+;; (yas/load-directory "~/emacsd/bundles/mustache/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.mustache$" . mustache-mode))
