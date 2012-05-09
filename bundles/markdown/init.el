39348f11be721c2aef38c032849a9490e6e43bea
Michael S. Klishin <michael@novemberain.com>
2012-05-10 01:57:43 +0400

    First markdown snippet

--- bundles/markdown/init.el
+++ bundles/markdown/init.el
@@ -1,6 +1,6 @@
 (require 'markdown-mode)
 
-;; (yas/load-directory "~/emacsd/bundles/markdown/snippets")
+(yas/load-directory "~/emacsd/bundles/markdown/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
