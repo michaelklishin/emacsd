12a63183555ebfbd58bac7857ff3bde9e75dafa8
Michael S. Klishin <michael@novemberain.com>
2012-06-03 16:42:13 +0400

    Enable paredit in the REPL

--- bundles/clojure/init.el
+++ bundles/clojure/init.el
@@ -28,8 +28,10 @@
 
 (require 'paredit)
 (paredit-mode)
+
 (autoload 'paredit-mode "paredit" "Minor mode for pseudo-structurally editing Lisp code." t)
 (add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
 (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'slime-repl-mode-hook       (lambda () (paredit-mode +1)))
