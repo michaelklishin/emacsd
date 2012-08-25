a3468709eb606acd3fc6ac7d3c06198fc800b7b6
Michael S. Klishin <michael@novemberain.com>
2012-08-25 17:00:33 +0400

    Use shell-script-mode for .zsh files

--- bundles/shell-script/init.el
+++ bundles/shell-script/init.el
@@ -1,7 +1,9 @@
 (yas/load-directory "~/emacsd/bundles/shell-script/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.sh$" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 
-(add-to-list 'auto-mode-alist '("\\zshenv$" . shell-script-mode))
-(add-to-list 'auto-mode-alist '("\\zprofile$" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\.zshrc$" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\.zshenv$" . shell-script-mode))
+
 
