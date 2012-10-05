c4bee51a8f0409302091ce7802a349aefcb6f7a6
Michael S. Klishin <michael@novemberain.com>
2012-10-05 16:08:48 +0400

    Remove code that used hardcoded tag table paths

--- bundles/erlang/init.el
+++ bundles/erlang/init.el
@@ -1,29 +1,21 @@
 ;; This is needed for Erlang mode setup
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R15B01/")
-(setq load-path (cons "/usr/local/Cellar/erlang/R15B01/lib/erlang/lib/tools-2.6.7/emacs/" load-path))
-(setq exec-path (cons "/usr/local/Cellar/erlang/R15B01/bin/" exec-path))
+(setq        erlang-root-dir (car (file-expand-wildcards "/usr/local/Cellar/erlang/R1*")))
+(add-to-list 'load-path      (car (file-expand-wildcards "/usr/local/Cellar/erlang/R*/lib/erlang/lib/tools-*/emacs")))
+(add-to-list 'exec-path      (car (file-expand-wildcards "/usr/local/Cellar/erlang/R*/bin")))
 
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R15B01")
+(setq erlang-root-dir (car (file-expand-wildcards "/usr/local/Cellar/erlang/R*")))
 
 (require 'erlang-start)
 (require 'erlang)
 
-;; This is needed for Distel setup
-;; (let ((distel-dir "~/Tools/emacsd.git/bundles/erlang/distel"))
-;;   (unless (member distel-dir load-path)
-;;     ;; Add distel-dir to the end of load-path
-;;     (setq load-path (append load-path (list distel-dir)))))
-
-
 (setq inferior-erlang-machine-options '("-sname" "emacs"))
 
-
-;; (load "distel/distel")
+;; (add-to-list 'load-path "~/emacsd/bundles/erlang/distel/")
 ;; (require 'distel)
 ;; (distel-setup)
 
 ;; load bundle snippets
-;; (yas/load-directory "~/Tools/emacsd.git/bundles/erlang/snippets/")
+(yas/load-directory "~/emacsd/bundles/erlang/snippets/")
 
 
 (add-to-list 'auto-mode-alist '("\\.src$" . erlang-mode))
