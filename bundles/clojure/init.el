12002262f6ec00ada44f2d56e18a56de70056349
Michael Klishin <michael@defprotocol.org>
2012-11-30 22:39:25 +0400

    Minor Clojure/Leiningen-related tweaks

--- bundles/clojure/init.el
+++ bundles/clojure/init.el
@@ -1,6 +1,9 @@
 ;; a workaround for a possible Emacs 23.1 bug. MK.
 (setq warning-suppress-types nil)
 
+;; for Leiningen to be found
+(add-to-list 'exec-path "~/bin/")
+
 (require 'clojure-mode)
 (require 'clojure-test-mode)
 
@@ -8,7 +11,9 @@
 (yas/load-directory "~/emacsd/bundles/clojure/snippets")
 
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-(add-hook 'clojure-mode-hook   (lambda () (highlight-parentheses-mode +1)))
+(add-hook 'clojure-mode-hook   (lambda ()
+                                 (highlight-parentheses-mode +1)
+                                 (setq buffer-save-without-query t)))
 
 
 ;;
