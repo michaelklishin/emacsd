a0033e4ff7ef5388305c381ff2c962faaabdf8e1
Michael Klishin <michael@defprotocol.org>
2013-02-18 12:32:47 +0400

    Move Emacs Lisp-specific bits to its own init file

--- bundles/emacs-lisp/init.el
+++ bundles/emacs-lisp/init.el
@@ -8,3 +8,7 @@
 (autoload 'highlight-parentheses-mode "highlight parenthesis"
   "Highlights parenthesis in Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook          (lambda () (highlight-parentheses-mode +1)))
+
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
