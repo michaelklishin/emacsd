2533c342bb4e5d06b881d85738bca11a41d2482b
Michael Klishin <michael@defprotocol.org>
2013-07-16 22:55:37 +0400

    Set default tab size for C#

--- bundles/csharp/init.el
+++ bundles/csharp/init.el
@@ -2,3 +2,9 @@
 (require 'csharp-mode)
 
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+
+
+(add-hook 'csharp-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode t)
+            (setq tab-width 8)))
