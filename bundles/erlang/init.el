f98b295e71b1bf2760f055d136667be3721d8f72
Michael Klishin <michael@clojurewerkz.org>
2014-02-17 16:14:36 +0400

    Don't untabify erlang-mode buffers automatically

--- bundles/erlang/init.el
+++ bundles/erlang/init.el
@@ -28,5 +28,6 @@
 (add-to-list 'auto-mode-alist '("\\.app$" . erlang-mode))
 (add-to-list 'auto-mode-alist '("rabbitmq.config" . erlang-mode))
 
-(add-hook 'erlang-mode-hook 'untabify-buffer t)
 (add-hook 'erlang-mode-hook 'highlight-parentheses-mode)
+(add-hook 'erlang-mode-hook (lambda ()
+                              (setq indent-tabs-mode nil)))
