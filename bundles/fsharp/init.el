53638fbde6bfed90f738286ee7908c191eae3756
Michael Klishin <michael@clojurewerkz.org>
2014-08-24 23:36:26 +0400

    Rely on fsharpc and fsharpi being in PATH

--- bundles/fsharp/init.el
+++ bundles/fsharp/init.el
@@ -5,5 +5,5 @@
 (autoload 'fsharp-mode "fsharp" "Major mode for editing F# code." t)
 (autoload 'run-fsharp "inf-fsharp" "Run an inferior F# process." t)
 
-(setq inferior-fsharp-program "mono /home/antares/Tools/fsharp/fsi.exe --readline-")
-(setq fsharp-compiler "mono /home/antares/Tools/fsharp/fsc.exe")
+(setq inferior-fsharp-program "fsharpi")
+(setq fsharp-compiler "fsharpc")
