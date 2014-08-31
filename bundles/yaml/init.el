f7e8512cb440e41d4e165e8139779e37d177b292
Michael Klishin <michael@clojurewerkz.org>
2014-08-31 19:59:02 +0400

    Use yaml-mode for .yaml files (e.g. cassandra.yaml)

--- bundles/yaml/init.el
+++ bundles/yaml/init.el
@@ -3,4 +3,5 @@
 ;; load bundle snippets
 ;; (yas/load-directory "~/emacsd/bundles/yaml/snippets")
 
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$"  . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
