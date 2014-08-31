a379d9d91f1d3a28634bbc81795c8c8069e04161
Michael Klishin <michael@clojurewerkz.org>
2014-08-31 19:58:46 +0400

    Create a scratch buffer for F#, too

--- initializers/scratches_initializer.el
+++ initializers/scratches_initializer.el
@@ -4,8 +4,13 @@
 (setq default-major-mode 'java-mode)
 (set-buffer-major-mode (get-buffer-create "*java-scratch*"))
 
+(setq default-major-mode 'fsharp-mode)
+(set-buffer-major-mode (get-buffer-create "*fsharp-scratch*"))
+
 (setq default-major-mode 'ruby-mode)
 (set-buffer-major-mode (get-buffer-create "*ruby-scratch*"))
 
 (setq default-major-mode 'org-mode)
 (set-buffer-major-mode (get-buffer-create "*org-scratch*"))
+
+(setq default-major-mode 'erlang-mode)
