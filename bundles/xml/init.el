bbcbf1b6e25694d4c700016d5096cb31d6780c6b
Michael Klishin <michael@clojurewerkz.org>
2015-02-19 14:38:17 +0300

    Clean up

--- bundles/xml/init.el
+++ bundles/xml/init.el
@@ -1,5 +1,2 @@
-;; load bundle snippets
-;; (yas/load-directory "~/emacsd/bundles/xml/snippets")
-
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.xml.inc$" . nxml-mode))
