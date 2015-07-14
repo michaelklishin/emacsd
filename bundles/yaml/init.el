973545b8dfe3b2af48e9ec96542e750461300514
Michael Klishin <michael@clojurewerkz.org>
2015-07-14 22:28:34 +0300

    ...and yml.erb

--- bundles/yaml/init.el
+++ bundles/yaml/init.el
@@ -5,4 +5,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.yml$"  . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml.erb$"  . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml.erb$" . yaml-mode))
