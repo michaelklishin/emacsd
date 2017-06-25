1843cb29681ae556deccb4306af1352b702121a0
Michael Klishin <michael@clojurewerkz.org>
2017-06-25 20:34:50 +0300

    More package archives

--- initializers/package_initializer.el
+++ initializers/package_initializer.el
@@ -1,5 +1,8 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
 (package-initialize)
 (package-refresh-contents)
