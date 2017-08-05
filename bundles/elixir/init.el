10e796f70c099a08e9e2342db994cb57733cef7b
Michael Klishin <michael@clojurewerkz.org>
2017-08-05 07:25:00 +0300

    Load YaSnippet files for Elixir

--- bundles/elixir/init.el
+++ bundles/elixir/init.el
@@ -1,2 +1,9 @@
 (require 'elixir-mode)
 (require 'elixir-smie)
+
+;; load bundle snippets
+(yas/load-directory "~/emacsd/bundles/elixir/snippets/")
+
+(add-hook 'elixir-mode-hook 'highlight-parentheses-mode)
+(add-hook 'elixir-mode-hook (lambda ()
+                              (setq indent-tabs-mode nil)))
