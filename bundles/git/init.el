6fac26b07255f126c938d429e68c3965f99305b9
Michael Klishin <michael@clojurewerkz.org>
2017-09-25 05:46:02 +0300

    Switch to magit from stable melpa

--- bundles/git/init.el
+++ bundles/git/init.el
@@ -1,9 +1,11 @@
 ;; Magit and git-* packages
 (unless (require 'magit nil 'dont-fail)
   (progn
+    (package-install 'with-editor)
     (package-install 'git-gutter-fringe+)
     (package-install 'magit)
     (package-install 'magithub)))
 
-(require 'git-modes)
-(require 'magit)
+(use-package magit
+             :ensure t
+             :pin melpa-stable)
--- initializers/package_initializer.el
+++ initializers/package_initializer.el
@@ -2,7 +2,14 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
 (package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
